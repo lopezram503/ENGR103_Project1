9143ab51664f4b83458080a4481abf0e5d719011
Ramiro Lopez <lopezram@oregonstate.edu>
2026-06-23 13:05:42 -0700

    updated animal.py assignment final

--- animal.py
+++ animal.py
@@ -2,7 +2,7 @@
 # GitHub username: lopezram503
 # Date: 6/23/2026
 # Description: Asks the user for their favorite animal and then
-#          prints out that is that that is their favorite animal
+#              prints out that that is their favorite animal
 
 fave_animal =  input('Please enter your favorite animal: ')
 print('Your favorite animal is the ' + fave_animal + '.')
